b22736234bb2608c48538b2a8ff09710b918ce96
raquelmv <raquel.mvillarreal@gmail.com>
2018-06-01 16:51:40 -0500

    btns created and link to java script

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,10 +2,79 @@
 // GLOBAL VARIABLES INDEX 
 var userName = "";
 var userPassword = "";
-var filterArray = ["diary", "chocolate","almonds"];
-var filterChoices =[];
+var filterArray = ["diary", "chocolate", "almonds"];
+var filterChoices = [];
+var otherFilterChoice = "";
 
-//#get_started button on-click event  takes you to create user 
+//#quick_start button on-click event  takes you to create user 
+buttonsRender();
+// rendering buttons
+function buttonsRender() {
+
+    for (var i = 0; i < filterArray.length; i++) {
+        var filterBtn = $("<button>");
+        filterBtn.addClass("#filter_render");
+        // Adding a data-attribute
+        filterBtn.attr("data-name", filterArray[i]);
+        // Providing the initial button text
+        filterBtn.text(filterArray[i]);
+        // Adding the button to the buttons-view div
+        $("#show_filterbtn").append(filterBtn);
+    }
+}
+
+/*
+$("#search-more").on("click", function (event) {
+    event.preventDefault();
+    // This line grabs the input from the textbox
+    var otherFilterChoice = $("#search_giphys").val().trim();
+
+    if (otherFilterChoice != "") {
+
+        celebritiesArray.push(newCelebrity);
+        console.log(celebritiesArray);
+        buttonsRender();
+    }
+});
+
+*/
+
+/// TRYOUT BTNS //
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.chips');
+    var instances = M.Chips.init(elems, options);
+  });
+var chip = {
+    tag: 'chip content',
+    image: '', //optional
+  };
+
+$('.chips').chips();
+$('.chips-initial').chips({
+    data: [{
+        tag: 'Apple',
+    }, {
+        tag: 'Microsoft',
+    }, {
+        tag: 'Google',
+    }],
+});
+$('.chips-placeholder').chips({
+    placeholder: 'Enter a tag',
+    secondaryPlaceholder: '+Tag',
+});
+$('.chips-autocomplete').chips({
+    autocompleteOptions: {
+        data: {
+            'Apple': null,
+            'Microsoft': null,
+            'Google': null
+        },
+        limit: Infinity,
+        minLength: 1
+    }
+});
+
 
 //#create_acount pop up window to fill in information + submit button jquery 
 
