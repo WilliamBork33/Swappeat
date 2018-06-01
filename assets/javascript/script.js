81cbaf4189920f6243662e1041798ea8b827f4a3
raquelmv <raquel.mvillarreal@gmail.com>
2018-05-31 21:37:41 -0500

    basic javascript pseudocode and font link

---
+++ assets/javascript/script.js
@@ -0,0 +1,18 @@
+
+// GLOBAL VARIABLES INDEX 
+var userName = "";
+var userPassword = "";
+var filterArray = ["diary", "chocolate","almonds"];
+var filterChoices =[];
+
+//#get_started button on-click event  takes you to create user 
+
+//#create_acount pop up window to fill in information + submit button jquery 
+
+//#login_btn function, recieves users password and user name input and takes you to the dashboard
+
+// jquery creates buttons for variable filterArray
+
+// Jquery creates buttons for others
+
+//jquery stores user choices in to filterChoices
