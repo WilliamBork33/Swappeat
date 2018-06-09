6652b4a5b6c7285732179d9bb13a4ba49e70cfe7
William Bork <williambork33@gmail.com>
2018-06-09 05:03:23 -0500

    Adding Login Page.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -75,4 +75,15 @@ $(document).on("click", "#searchBtn", function (event) {
     });
 
 
+
+
+function login(showhide) {
+    if(showhide == "show"){
+    document.getElementById('popupbox').style.visibility="visible";
+    }
+    else if (showhide == "hide") {
+    document.getElementById('popupbox').style.visibility="hidden"; 
+}
+}
+
 });
